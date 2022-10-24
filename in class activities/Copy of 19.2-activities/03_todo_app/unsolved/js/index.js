48a003576bc62507d4797d8d2a5cfac3757f18c3
myles4wiles <mylesdickens@outlook.com>
2022-10-24 12:11:11 -0500

    not sure
    not sure what this is all about

---
+++ in class activities/Copy of 19.2-activities/03_todo_app/unsolved/js/index.js
@@ -0,0 +1,10 @@
+// Create event listener to enter items below
+$("enter").on("click", function(){
+    var task =$("#todoItem").val();
+    
+  });
+  
+
+// Create event listener to delete items below
+
+// Create event listener to toggle 
